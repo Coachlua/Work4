ce87d7cab2a0ad34291787452e320e5d5a088311
User <User@RYZEN>
2024-01-09 13:15:51 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Выведите на экран такой текст
+Console.WriteLine("Two roads diwerged in a yellow wood");
+Console.WriteLine("And sorry i could not travel both");
+Console.WriteLine("And be one traveler, long i stood");
+Console.WriteLine("And looked down one as far as i could");
+Console.WriteLine("To where it bent in the undergrowth");
+// 2. Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними
+Console.WriteLine($"{1} {13} {49}");
+// 3. Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними
+Console.WriteLine("Введите три числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine($"{5} {10} {15}");
